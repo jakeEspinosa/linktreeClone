8b374b38ee15e2d2dd847b45885b069a940a2c9e
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-10-13 13:47:48 -0700

    Add grid.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import LinkyProfilePicture from "@/components/LinkyProfilePicture.vue";
 </script>
 
 <template>
+  <div class="grid">
     <div class="pfp">
         <LinkyProfilePicture image-url="src/assets/images/Espinosa.jpg"/>
     </div>
@@ -16,19 +17,32 @@ import LinkyProfilePicture from "@/components/LinkyProfilePicture.vue";
     <footer class="footer">
       <h2>footer</h2>
     </footer>
+  </div>
 </template>
 
 <style scoped>
+.grid {
+  height: 100%;
+  display: grid;
+  grid-template-areas: "header"
+                       "main"
+                       "footer";
+}
+
 .pfp {
+  grid-area: "header";
   padding-top: 1rem;
+  height: 90%;
 }
 
 .main {
+  grid-area: "main";
   min-height: 75%;
   padding-top: 1rem;
 }
 
 .footer {
+  grid-area: "footer";
   height: 10%;
 }
 </style>
