ba329d02749853d76498e57c6c4278818e964341
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-10-13 22:16:36 -0700

    Finalize layout.

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,24 @@ import LinkyProfilePicture from "@/components/LinkyProfilePicture.vue";
 <template>
   <header class="header">
     <LinkyProfilePicture image-url="src/assets/images/Espinosa.jpg"/>
-    <h1>Full stack software engineer with a passion for accessibility</h1>
+    <h1>Full stack software engineer passionate about learning</h1>
 </header>
   <main class="main">
-    <button>buttons</button>
-    <button>buttons</button>
-    <button>buttons</button>
-    <button>buttons</button>
+    <a href="https://www.jakeespinosa.com/blog" target="_blank">
+      <button>
+        Blog
+      </button>
+    </a>
+    <a href="https://github.com/jakeEspinosa" target="_blank">
+      <button>
+        GitHub
+      </button>
+    </a>
+    <a href="https://www.linkedin.com/in/jake-espinosa/" target="_blank">
+      <button>
+        LinkedIn
+      </button>
+    </a>
   </main>
   <footer class="footer">
     <h2>© 2023 Espinosa Enterprises, LLC. All Rights Reserved.</h2>
@@ -33,6 +44,7 @@ import LinkyProfilePicture from "@/components/LinkyProfilePicture.vue";
 
 .main {
   min-height: 60dvh;
+  gap: 1.5rem;
 }
 
 .footer {
@@ -42,12 +54,12 @@ h1, h2 {
   width: 40ch;
   text-align: center;
   font-style: italic;
-  color: white;
+  color: #f5e9e2;
 }
 
 button {
-  color: white;
-  background-color: #7E04E9;
+  color: #0b0014;
+  background-color: #f5e9e2;
   width: 40ch;
   border-radius: 10px;
   height: 10ch;
